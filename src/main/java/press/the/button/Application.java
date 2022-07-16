375f2d4848384d6497debfa9b9f209e45365a6cf
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2022-07-16 22:35:44 +0200

    added scanner and countdown to application - still problems with the threading

--- src/main/java/press/the/button/Application.java
+++ src/main/java/press/the/button/Application.java
@@ -1,10 +1,16 @@
 package press.the.button;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 public class Application {
 
-    public static void main(String[] args) {
-        System.out.print("Hello World!");
 
+    public static void main(String[] args) {
+        UserInputReader userInputReader = new UserInputReader();
+        userInputReader.readForActivity();
     }
 
 
---
+++ src/main/java/press/the/button/UserInputReader.java
@@ -0,0 +1,62 @@
+package press.the.button;
+
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class UserInputReader {
+
+    private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    private int countdown = 10;
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public void readForActivity() {
+
+        String keyInput = "";
+        while (!keyInput.toLowerCase().equals("lost")) {
+            restartCountdown();
+            printRetryText();
+            keyInput = scanner.nextLine();
+            scheduler.shutdown();
+            countdown = 10;
+        }
+        System.out.println("Correct password. You broke the loop. Good job Hurley, you are free now.");
+    }
+
+    private void restartCountdown() {
+
+        Runnable runnable = new Runnable() {
+
+            public void run() {
+
+                System.out.println(countdown);
+                countdown--;
+
+                if (countdown <= 0) {
+                    universalShutdown();
+                }
+            }
+        };
+        scheduler.scheduleAtFixedRate(runnable, 0, 1, SECONDS);
+    }
+
+    private void printRetryText() {
+        System.out.println("Wrong password. Countdown reset! ENTER the correct password.");
+    }
+
+    private void printCountdownText() {
+        System.out.println(countdown);
+    }
+
+    private void universalShutdown() {
+        scanner.close();
+        scheduler.shutdown();
+        System.out.println("You did not save the universe. Too bad Hurley.");
+
+    }
+
+}
