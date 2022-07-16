ff10e1a9fa3716e02e66dbbef210557fcfdfc6d9
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2022-07-16 21:26:39 +0200

    add maven project with hello world and test

---
+++ src/main/java/press/the/button/Application.java
@@ -0,0 +1,11 @@
+package press.the.button;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+
+    }
+
+
+}
---
+++ src/test/java/press/the/button/ApplicationTest.java
@@ -0,0 +1,13 @@
+package press.the.button;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void mainTest() {
+        Application.main(new String[0]);
+
+    }
+
+}
